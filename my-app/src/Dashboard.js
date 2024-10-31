5490504d7e0fb240b019e4c2c7fe38a6eda39e36
Clotted <junealsk@naver.com>
2024-10-31 09:26:22 +0900

    대시보드 임시로 작성

---
+++ my-app/src/Dashboard.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Dashboard() {
+  return (
+    <div className="container">
+      <h1>대시보드</h1>
+      <p>환영합니다! 여기에서 사용자 정보를 관리할 수 있습니다.</p>
+    </div>
+  );
+}
+
+export default Dashboard;
